0fde048f6c9b35ec652357903df5ac2869553b90
NicklasSpendler <KN6s~?c$%+T>
2021-10-14 14:07:26 +0200

    write out acitivity and fetch

--- js/main.js
+++ js/main.js
@@ -1,1 +1,52 @@
-fetch("http://localhost:8080/equipment").then(response => response.json()).then(result => console.log(result));
+document.addEventListener('DOMContentLoaded', init());
+
+function init() {
+    initActivityTable();
+}
+
+
+function initActivityTable(){
+    fetch("http://localhost:8080/activity")
+        .then(response => response.json())
+        .then(result => renderAcitivtyTable(result));
+    
+        ;
+}
+
+function renderAcitivtyTable(result) {
+    let dataTable = document.querySelector(".data__table");
+
+    let tableItemNode = document.querySelector(".table__item_clonenode");
+    
+
+    result.forEach(activity => {
+        let cloneNode = tableItemNode.cloneNode(true);
+
+        cloneNode.classList.remove("hidden");
+
+        let nodeID = cloneNode.querySelector(".item__id p")
+        nodeID.textContent = activity.activityID;
+    
+        let nodeName = cloneNode.querySelector(".item__name p")
+        nodeName.textContent = activity.name;
+    
+        let nodeprice = cloneNode.querySelector(".item__price p")
+        nodeprice.textContent = activity.price;
+    
+        let nodemin_age = cloneNode.querySelector(".item__min_age p")
+        nodemin_age.textContent = activity.min_age;
+    
+        let nodemin_height = cloneNode.querySelector(".item__min_height p")
+        nodemin_height.textContent = activity.min_height;
+    
+        let nodemax_participants = cloneNode.querySelector(".item__max_participants p")
+        nodemax_participants.textContent = activity.participants;
+        
+        
+
+        dataTable.appendChild(cloneNode);
+    });
+
+
+
+}
