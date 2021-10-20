94716be6c1c004b7bb5cc2f273c7b0766ca021b5
NicklasSpendler <KN6s~?c$%+T>
2021-10-20 18:17:52 +0200

    Made edit for activities

--- js/editActivity.js
+++ js/editActivity.js
@@ -1,8 +1,5 @@
 init();
 
-console.log('test');
-
-
 function init() {
     fetchData();
 }
@@ -17,5 +14,58 @@ function getActivityID() {
 function fetchData() {
     fetch(`http://localhost:8080/activity/${getActivityID()}`)
     .then(response => response.json())
-    .then(result => console.log(result));
+    .then(result => setData(result));
 }
+
+function setData(data){
+    console.log(data);
+
+    let editForm__nameElem = document.querySelector(".editForm__name");
+    let editForm__priceElem = document.querySelector(".editForm__price");
+    let editForm__ageElem = document.querySelector(".editForm__age");
+    let editForm__heightElem = document.querySelector(".editForm__height");
+    let editForm__participantsElem = document.querySelector(".editForm__participants");
+
+    editForm__nameElem.value = data.name;
+    editForm__priceElem.value = data.price;
+    editForm__ageElem.value = data.min_age;
+    editForm__heightElem.value = data.min_height;
+    editForm__participantsElem.value = data.participants;
+}
+
+
+document.querySelector(".editForm__submit").addEventListener("click", async function (e) {
+  e.preventDefault()
+  // ta' fat i formen fra html'en
+  const form = document.querySelector("#frmEditActivity")
+  const acitivtyID = getActivityID();
+  const urlEditEquipment = "http://localhost:8080/activity/"+acitivtyID;
+
+  try {
+    const formData = new FormData(form);
+    const plainFormData = Object.fromEntries(formData.entries());
+    const jsonString = JSON.stringify(plainFormData);
+    console.log(jsonString);
+    const fetchOptions = {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: jsonString
+    }
+    const response = await fetch(urlEditEquipment, fetchOptions);
+    if (!response.ok) {
+      console.log("det gik ikke godt");
+    } else {
+      console.log("det gik godt")
+      //document.querySelector(".msg").innerHTML = "Udstyr er opdateret. Du vil blive ledt tilbage til den forrige side om 3 sek."
+      setTimeout(function () {
+        location.href = "../index.html";
+      }, 3000)
+    }
+
+  } catch (error) {
+    alert(error.message);
+    console.log(error);
+  }
+})
