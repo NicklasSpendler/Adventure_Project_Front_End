479133cbe941336c2b632fda7653675e1a2d7b2f
Sebistorm <seba7843@stud.kea.dk>
2021-10-21 17:12:12 +0200

    reservation + meget lidt css

--- js/createReservation.js
+++ js/createReservation.js
@@ -1,50 +1,91 @@
 document.addEventListener('DOMContentLoaded', init());
-
 function init() {
   initActivityTable();
   console.log("test")
 }
 
 document.querySelector("#btnSearchEvents").addEventListener("click", async function (e) {
-  console.log("søg efter events")
   let activity = document.querySelector("#sltActivity");
+  // hent alt udstyret
 
-
+  fetch("http://localhost:8080/findEventByName/" + activity.value)
+    .then(response => response.json())
+    .then(result => renderEventTable(result));
+  ;
 
 })
 
-/*
-document.querySelector("#frmCreateReservation").addEventListener("submit", async function (e) {
-  e.preventDefault();
-
-  const form = document.querySelector("#frmCreateReservation");
-  const createReservationUrl = form.action;
-
-  try {
-    const formData = new formData(form);
-    const plainformdata = Object.fromEntries(formData.entries);
-    const jsonString = JSON.stringify(plainformdata)
-
-    const fetchOptions = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: jsonString
+function renderEventTable(result){
+  if(document.querySelector(".event-item")) {
+    let deleteDivs = document.querySelectorAll(".event-item")
+    for (i = 0; i < deleteDivs.length; i++) {
+      deleteDivs[i].remove();
     }
-
-    const response = await fetch(createReservationUrl, fetchOptions);
-    if(!response.ok) {
-      console.log('Det var satans >:(')
-    }
-
-  } catch (error) {
-    alert(error)
-    console.log(error)
   }
+
+  let eventContainer = document.querySelector(".eventsContainer");
+  result.forEach(event =>{
+    let date = new Date(event.date);
+    let eventItem = `<div class="event-item d-flex form-check">
+                            <div class="col-sm-3">
+                                <input class="form-check-input" type="radio" name="ChooseEvent" required value="${event.eventID}">
+                                ${event.eventActivity}
+                            </div>
+                            <p class="col-sm-3">${event.timeSlot}</p>
+                            <p class="col-sm-3">${event.maxParticipants}</p>
+                            <p class="col-sm-3">${date.getDate()}-${date.getMonth()}-${date.getFullYear()}</p>
+                     </div>`;
+    eventContainer.insertAdjacentHTML("afterend", eventItem);
+  });
+}
+
+// create Reservation
+document.querySelector("#frmCreateReservation").addEventListener("submit",function(e) {
+
+  e.preventDefault();
+  console.log("create reservation")
+  if(document.querySelector('input[name="ChooseEvent"]:checked')) {
+    let eventID = document.querySelector('input[name="ChooseEvent"]:checked').value;
+    console.log(eventID);
+    document.querySelector('input[name="eventID"]').value = eventID;
+    postReservationToBackend();
+  } else {
+    document.querySelector(".msg").innerHTML = "Husk at vælg et Event"
+  }
+
 })
 
- */
+async function postReservationToBackend () {
+    const form = document.querySelector("#frmCreateReservation");
+    console.log(form)
+    const createReservationUrl = form.action;
+    try {
+      const formData = new FormData(form);
+      const plainformdata = Object.fromEntries(formData.entries());
+      const jsonString = JSON.stringify(plainformdata)
+
+      const fetchOptions = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: jsonString
+      }
+
+      const response = await fetch(createReservationUrl, fetchOptions);
+      if(!response.ok) {
+        console.log('Det var satans >:(')
+      }
+
+    } catch (error) {
+      alert(error)
+      console.log(error)
+    }
+}
+
+
+
+
 
 // get activies - Select
 function initActivityTable(){
