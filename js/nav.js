6cf5dc8ea669a3c711488ba0a39ae76d126be494
Sebistorm <seba7843@stud.kea.dk>
2021-10-22 10:54:57 +0200

    navbar html + js

---
+++ js/nav.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', init());
+function init() {
+  navbar()
+}
+
+function navbar() {
+  console.log("navbar")
+  const nav = document.querySelector("#navbar");
+  const navbar = `<div class="topnav">
+                        <link rel="stylesheet" href="css/main.css">
+                        <a href="/404.html" class="">FORSIDE</a>
+                        <a href="index.html">Events</a>
+                        <a href="CreateReservation.html">Lav reservation</a>
+                        <a href="activity.html">Aktiviteter</a>
+                        <a href="equipment.html">Udstyr</a>
+                        <a href="Employee.html">Medarbejdere</a>
+                  </div>`;
+  nav.insertAdjacentHTML("afterend", navbar);
+}
